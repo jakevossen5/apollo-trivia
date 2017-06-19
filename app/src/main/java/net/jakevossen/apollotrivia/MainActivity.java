93a1f57c95f50c1b2fcf38cdec006d473495a54f
Jake Vossen <jakvossen5@gmail.com>
2017-06-18 18:13:58 -0600

    added menu and fixed layout

--- app/src/main/java/net/jakevossen/apollotrivia/MainActivity.java
+++ app/src/main/java/net/jakevossen/apollotrivia/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -20,6 +23,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         genNewQuestion();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.info:
+                showInfo();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void showInfo() {
+        Intent intent = new Intent(this, Info.class);
+        startActivity(intent);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
--- app/src/main/java/net/jakevossen/apollotrivia/explanation.java
+++ app/src/main/java/net/jakevossen/apollotrivia/explanation.java
@@ -3,6 +3,9 @@ package net.jakevossen.apollotrivia;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.TextView;
@@ -24,6 +27,30 @@ public class explanation extends AppCompatActivity {
 
         setExplanation();
         setSource();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.info:
+                showInfo();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void showInfo() {
+        Intent intent = new Intent(this, Info.class);
+        startActivity(intent);
     }
 
     private void setSource() {
